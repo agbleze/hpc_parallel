28bac4e953d2293ca8e34c16a97e45973df5d5f6
linus <lagbleze@st.ug.edu.gh>
2026-03-31 23:06:30 +0200

    feat: add gather MPI collective communication mechanism

---
+++ gathering_colcom.py
@@ -0,0 +1,16 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
+
+if rank == 0:
+    data = "AAA"
+if rank == 1:
+    data = "BBB"
+if rank == 2:
+    data = "CCC"
+if rank == 3:
+    data = "DDD"
+array = comm.gather(data, root=0)
+
+if rank == 0:
+    print(f"The new array is {array}")
