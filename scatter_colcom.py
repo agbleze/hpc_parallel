14ec10f74a20b7d218ab623730e0470b292f8323
linus <lagbleze@st.ug.edu.gh>
2026-04-04 16:28:18 +0200

    feat: add scatter as collective communication

---
+++ scatter_colcom.py
@@ -0,0 +1,10 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
+if rank == 0:
+    array = ["AAA", "BBB", "CCC", "DDD"]
+else:
+    array = None
+
+data = comm.scatter(array, root=0)
+print(f"Process {rank} is working on {data}")
