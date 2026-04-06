687d6d15a2deb2e3e0ce9c15449146de32da51c5
linus <lagbleze@st.ug.edu.gh>
2026-04-06 20:12:16 +0200

    feat: add dask example

--- dask_example.py
+++ dask_example.py
@@ -1,7 +1,65 @@
+
+#%%
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
 import dask.array as da
 import dask.bag as db
+import dask
+
+#%%
+df = pd.DataFrame(np.random.rand(60).reshape(10,6))
+
+print(df)
+
+#%%
+ddf = dd.from_pandas(df, npartitions=4)
+
+print(ddf)
+
+print(ddf.divisions)
+
+#%%
+#print(ddf.compute())
+
+#%%
+narr = np.random.randint(0, 10, 100).reshape(10, 10)
+darr = da.from_array(narr, chunks=(5,5))
+print(f"darr: {darr}")
+print(f"array chunks: {darr.chunks}")
+
+print(f"Array: {darr.compute()}")
+
+#%% methods on collections
+print(ddf.columns)
+print(f"ddf[1]: {ddf[1].compute()}")
+m = ddf[1].mean()
+
+print(f"mean lazy: {m}")
+print(f"array mean: {m.compute()}")
+
+
+mm = np.mean(darr)
+print(f"mm: {mm}")
+print(f"mm.compute: {mm.compute()}")
+
+print(f"mm task graph: {mm.dask}")
+
+#%%
+print(f"visualize task graph: {mm.visualize()}")
+
+
+#%% dask delayed for creating custom parallelizable functions
+
+@dask.delayed
+def doubled(x):
+    return 2*x
+
+d = doubled(darr)
+
+print(f"delayed dask called: {d}")
+
+print(f"d.compute(): {d.compute()}")
+
 
 
